70620d4b3370ceadaa2c951af55d9919900558ec
ulkata <j.mihalkov2019@pmg-pernik.com>
2023-03-09 10:18:16 +0200

    pull

--- ListNumsActions/ListNumsActions/Program.cs
+++ ListNumsActions/ListNumsActions/Program.cs
@@ -23,7 +23,9 @@ namespace ListNumsActions
                         //TODO
                         break;
                     case "del":
-                        //TODO
+                        
+                        
+
                         break;
                     case "contains":
                         //TODO
